5ed8903c6119e208989f3ac4dd30a3ac61318b4a
Jyothis P <jyothisp52@gmail.com>
2020-05-28 01:57:19 +0530

    updated query for nearby trips.

--- api/trip/controllers/trip.js
+++ api/trip/controllers/trip.js
@@ -10,9 +10,29 @@ const π = Math.PI;
  * to customize this controller
  */
 
+
+function toRad(theta) {
+    return Math.PI * theta / 180;
+}
+
+
+function convert(la, lo, bias) {
+    la = toRad(la)
+    lo = toRad(lo)
+    // console.log('Radian => ', la, lo, centre);
+    let x = R * lo * bias;
+    let y = R * la
+    console.log('Cartesian: ', x, y);
+    return [x, y];
+}
+
+function dist(x, y, x1, y1) {
+    let d = Math.sqrt(Math.pow((x1 - x), 2) + Math.pow((y1 - y), 2))
+    return d;
+}
+
+
 function getParams(lat, lon, radius) {
-    lat = parseFloat(lat);
-    lon = parseFloat(lon);
     radius = parseFloat(radius);
     const params = {
         minLat: lat - radius / R * 180 / π,
@@ -23,21 +43,53 @@ function getParams(lat, lon, radius) {
     return params;
 }
 
+
 module.exports = {
     async near(ctx) {
-        let { lat, lon, radius } = ctx.query;
-        if (!radius)
-            radius = 4;
-        if (!lat || !lon) {
+        let { lat, lon, lat_d, lon_d } = ctx.query;
+        if (!lat || !lon || !lat_d || !lon_d) {
             ctx.response.status = 400;
             ctx.response.error = 'Bad request.'
             let error_pack = {
                 status: 400,
                 error: 'Bad request.',
-                message: 'Coordinates not supplied with the request.'
+                message: 'Coordinates not supplied with the request. You should supply lat, lon, lat_d and lon_d.'
             }
             return error_pack;
         }
+        lat = parseFloat(lat);
+        lon = parseFloat(lon);
+        lat_d = parseFloat(lat_d);
+        lon_d = parseFloat(lon_d);
+
+        let lat_offset = lat + lat_d;
+        let lon_offset = lon + lon_d;
+
+        let bias = Math.cos(lat);
+
+        let l = convert(lat, lon, bias);
+        let x = l[0]
+        let y = l[1]
+
+        l = convert(lat_offset, lon, bias);
+        let x1 = l[0]
+        let y1 = l[1]
+
+        let h_d = dist(x, y, x1, y1);
+
+        l = convert(lat, lon, bias);
+        x = l[0]
+        y = l[1]
+
+        l = convert(lat, lon_offset, bias);
+        x1 = l[0]
+        y1 = l[1]
+
+        let v_d = dist(x, y, x1, y1);
+
+
+        let radius = (v_d > h_d) ? v_d : h_d;
+
         let params = getParams(lat, lon, radius);
         console.log(lat, lon, radius)
         console.log(params)
