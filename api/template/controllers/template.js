be8734fe99d479fe6a72e2b3444f8bbb37c5a699
Jyothis P <jyothisp52@gmail.com>
2020-06-04 11:09:09 +0530

    added deep populate route.

--- api/template/controllers/template.js
+++ api/template/controllers/template.js
@@ -1,8 +1,28 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    /**
+ * Retrieve records.
+ *
+ * @return {Array}
+ */
+
+    async getDeep(ctx) {
+        if (ctx.query._q) {
+            return strapi.services.template.search(ctx.query);
+        }
+        return strapi.services.template.find(ctx.query, {
+            path: 'route',
+            populate: {
+                path: 'stops'
+            }
+        });
+    },
+};
