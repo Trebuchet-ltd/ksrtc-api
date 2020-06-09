1b931107af15fc824c01bfff0840fe25f96abb5e
Jyothis P <jyothisp52@gmail.com>
2020-06-10 01:03:08 +0530

    add checkIn route

--- api/attendance/controllers/attendance.js
+++ api/attendance/controllers/attendance.js
@@ -1,8 +1,111 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+const R = 6371; // earth's mean radius in km
+const sin = Math.sin, cos = Math.cos, acos = Math.acos;
+const π = Math.PI;
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+
+
+function toRad(theta) {
+    return π * theta / 180;
+}
+
+
+function convert(la, lo, bias) {
+    la = toRad(la)
+    lo = toRad(lo)
+    // console.log('Radian => ', la, lo, centre);
+    let x = R * lo * bias;
+    let y = R * la
+    return [x, y];
+}
+
+
+function dist(x, y, x1, y1) {
+    let d = Math.sqrt(Math.pow((x1 - x), 2) + Math.pow((y1 - y), 2))
+    return d;
+}
+
+
+module.exports = {
+
+    async checkin(ctx) {
+
+        if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+            try {
+                const { id, isAdmin = false } = await strapi.plugins[
+                    'users-permissions'
+                ].services.jwt.getToken(ctx);
+
+                const user = ctx.state.user;
+
+
+                let lat = ctx.request.body.lat;
+                let lon = ctx.request.body.lon;
+
+                lat = parseFloat(lat);
+                lon = parseFloat(lon);
+
+
+                let hub = await strapi.query('hub').findOne({ id: user.hub });
+
+                let h_lat = hub.lat;
+                let h_lon = hub.long;
+
+                let bias = cos(lat + (h_lat - lat));
+
+                let l = convert(lat, lon, bias);
+                let x = l[0]
+                let y = l[1]
+
+                l = convert(h_lat, h_lon, bias);
+                let x1 = l[0]
+                let y1 = l[1]
+
+                let d = dist(x, y, x1, y1);
+
+                console.log('Distance', d);
+
+                if (d > 2) {
+                    ctx.response.status = 406;
+                    ctx.response.error = 'Not Acceptable.'
+                    let error_pack = {
+                        status: 406,
+                        error: 'Not Acceptable.',
+                        message: 'You should be within your hub limits to check in.'
+                    }
+                    return error_pack;
+                } else {
+
+                    let data = {
+                        user: id,
+                        time: new Date(),
+                        hub: user.hub
+                    }
+
+                    let entity = await strapi.query('attendance').create(data);
+
+                    return entity;
+
+                }
+
+
+            } catch (err) {
+                // It will be there!
+                console.log(err)
+                ctx.response.status = 500;
+                let error_pack = {
+                    status: 500
+                }
+                return error_pack;
+
+            }
+        }
+    }
+};
