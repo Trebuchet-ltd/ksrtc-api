a9fb776d6e6832fb746ba3acde935a305a242808
Jyothis P <jyothisp52@gmail.com>
2020-06-30 11:23:53 +0530

    No duplication of hubs

--- api/hub/controllers/hub.js
+++ api/hub/controllers/hub.js
@@ -5,4 +5,37 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+
+function handleError(ctx, error, status = 500, message = 'Internal Server Error') {
+    ctx.response.status = status;
+    console.log('Error Object:', error);
+    console.log('Message:', message);
+    let error_pack = {
+        status: status,
+        message: message
+    }
+    return error_pack;
+}
+
+
+module.exports = {
+    /**
+     * Create a record.
+     *
+     * @return {Object}
+     */
+
+    async create(ctx) {
+
+        let duplicate = await strapi.query('hub').findOne(ctx.request.body);
+
+        if (duplicate)
+            return handleError(ctx, null, 409, 'Hub already Exists.');
+
+        let entity = await strapi.services.hub.create(ctx.request.body);
+
+        return sanitizeEntity(entity, { model: strapi.models.hub });
+    },
+};
